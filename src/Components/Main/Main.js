3f9c6813d9ee87d3758ea4a041609b314ea37b5f
Marc S <marcsalaver@gmail.com>
2022-09-21 12:00:54 -0700

    discord api requests dusted

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -7,17 +7,14 @@ import axios from "axios";
 var qs = require("qs");
 
 export default function Main() {
+  useEffect(() => {
+    getInfo();
+  }, []);
+
   const getInfo = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const codeValue = urlParams.get("code");
-    const params = new URLSearchParams();
-    params.append("client_id", process.env.REACT_APP_CLIENT_ID);
-    params.append("client_secret", process.env.REACT_APP_CLIENT_SECRET);
-    params.append("grant_type", "authorization_code");
-    params.append("code", codeValue);
-    params.append("redirect_uri", process.env.REACT_APP_CLIENT_REDIRECT);
-    params.append("scope", "identify email");
 
     var data = qs.stringify({
       client_id: process.env.REACT_APP_CLIENT_ID,
@@ -33,7 +30,7 @@ export default function Main() {
         Authorization: "Bearer qvfZPybqVKttGBYx0VmQc4GE6QKToG",
         "Content-Type": "application/x-www-form-urlencoded",
         Cookie:
-          "__cfruid=95b06f8eb82625d25ea743bede73536522e8ffd1-1663703266; __dcfduid=584b781e32e011edb6f4aa924745bf25; __sdcfduid=584b781e32e011edb6f4aa924745bf25f66a6891db401d468350f95b613e93be417523e6ca8a1c8d589f9ce91608f6c3",
+          "__cfruid=6875c96d8cb98269333cc2908d16da0c2afd1605-1663781576; __dcfduid=584b781e32e011edb6f4aa924745bf25; __sdcfduid=584b781e32e011edb6f4aa924745bf25f66a6891db401d468350f95b613e93be417523e6ca8a1c8d589f9ce91608f6c3",
       },
       data: data,
     };
@@ -42,6 +39,26 @@ export default function Main() {
       .then(function (response) {
         console.log(JSON.stringify(response.data));
         console.log("response", response);
+        console.log("response.data", response.data);
+        var data = qs.stringify({});
+        var config = {
+          method: "get",
+          url: "https://discord.com/api/users/@me",
+          headers: {
+            Authorization: `Bearer ${response.data.access_token}`,
+            // Cookie:
+            //   "__dcfduid=584b781e32e011edb6f4aa924745bf25; __sdcfduid=584b781e32e011edb6f4aa924745bf25f66a6891db401d468350f95b613e93be417523e6ca8a1c8d589f9ce91608f6c3",
+          },
+          data: data,
+        };
+
+        axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
         //response.data.access_token is the id for users logging in.
         //refresh token is something else i don't fully undrestand yet.
       })
@@ -58,7 +75,10 @@ export default function Main() {
             <Col id="appTitle">
               <h1>
                 <p className="pixel">Pixel</p>
-                <div id="info" onClick={getInfo}>
+                <div
+                  id="info"
+                  onClick={console.log("this used to be a button")}
+                >
                   Hoi!
                 </div>
               </h1>
